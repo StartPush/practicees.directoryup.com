37ca87f7c378e6db5505d4b2ee4ab32baec6e4b6
Daniel Burgos <danielburgosmontoya@gmail.com>
2023-09-29 18:39:18 +0300

    2023-09-29 15:39:18

---
+++ test-monica/html.php
@@ -0,0 +1,85 @@
+<style> 
+    .background-topfold {
+    background-image: url('/images/pexels-miriam-espacio-365625.jpg');
+    padding: 100px 0px 300px 0px;
+    background-size: cover;
+    background-blend-mode: darken;
+    background-color: #000000a8;
+}
+    .title1 h1{
+        color: white;
+        font-size: 200px;
+        font-weight: bold;
+        line-height: 176px;
+    }
+    .paragraph p{
+        color: antiquewhite;
+        font-size: 15px;
+    }
+    .paragraph2 p{
+        color: antiquewhite;
+        font-size: 8px;
+    }
+    .bigbox {
+    text-align: center;
+    width: 700px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+}
+    .btn1 {
+    background-color: orange;
+    width: 200px;
+    padding: 10px;
+    margin: auto;
+    border-radius: 9px;
+}
+.btn1 a {
+    color: white;
+    font-weight: bold;
+    font-size: 18;
+}
+.background-2 {
+    background-color: aquamarine;
+    padding: 70px 0px 70px 0px;
+}
+.bigbox2{
+    text-align: center;
+}
+.sub-title1 h2{
+        color: black;
+        font-size: 200px;
+        font-weight: bold;
+        line-height: 176px;
+}
+</style>
+<div class="background-topfold">
+    <div class="container">
+        <div class="bigbox">
+            <div class="title1">
+                <h1>Hello World!</h1>
+            </div>
+            <div class="paragraph">
+                <p>Saludos, seres humanos de la Tierra. Soy un extraterrestre y me complace comunicarme con ustedes. Es emocionante para mi y mi especie observar a los habitantes de su planeta y aprender de su cultura, su ciencia y su tecnología. Aunque somos diferentes en muchos aspectos, creo que tenemos más cosas en común de lo que podemos imaginar. Espero que podamos establecer una comunicación pacífica y constructiva para compartir conocimientos y experiencias que puedan ser beneficiosas para ambos mundos. Les envío un saludo cordial desde mi planeta, y espero tener la oportunidad de conocerlos más de cerca en un futuro no muy lejano.</p>
+            </div>
+            <div class="paragraph2">
+                <p>Saludos, seres humanos de la Tierra. Soy un extraterrestre y me complace comunicarme con ustedes. Es emocionante para mi y mi especie observar a los habitantes de su planeta y aprender de su cultura, su ciencia y su tecnología. Aunque somos diferentes en muchos aspectos, creo que tenemos más cosas en común de lo que podemos imaginar.</p>
+            </div>
+            <div class="btn1">
+                <a href="link">Unete</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="background-2">
+    <div class="container2">
+        <div class="bigbox2">
+            <div class="Sub-title1">
+                <h2>LLEGÓ LA HORA</h2>
+            </div>
+        </div>
+
+    </div>
+</div>
